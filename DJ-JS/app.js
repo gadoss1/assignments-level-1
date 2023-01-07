d544aa9ab11fbdb5546d180b0eff58be1a8c3292
Gregory Doss <gadoss83@yahoo.com>
2023-01-07 12:07:19 -0500

    Updated code function V3

--- DJ-JS/app.js
+++ DJ-JS/app.js
@@ -5,49 +5,48 @@ const square = document.querySelector(".square");
 document.body.style.backgroundColor = "grey";
 
 square.addEventListener("mouseover", function(){
-    square.style.backgroundColor= "turquoise";
+    square.style.backgroundColor= "blue";
     console.log ("Back to original");
-})
-
-square.addEventListener("click", function () {
-square.style.backgroundColor = "blue";
-console.log ("DJ Blue");
 })
 
 square.addEventListener("mousedown", function(){
     square.style.backgroundColor= "red";
     console.log ("DJ Red");
 })
-
-square.addEventListener("mouseleave", function(){
+square.addEventListener("mouseup", function(){
     square.style.backgroundColor= "yellow";
     console.log ("DJ Yellow");
 })
-
 square.addEventListener("dblclick", function(){
     square.style.backgroundColor= "green";
     console.log ("DJ Green");
 })
-
-document.body.addEventListener("scrollover", function(){
+document.body.addEventListener("wheel", function(){
     square.style.backgroundColor= "orange";
     console.log ("DJ Orange");
 })
 
+square.addEventListener("mouseleave", function(){
+    square.style.backgroundColor= "turquoise";
+    console.log ("Original Color");
+})
 
-document.body.addEventListener("keyDown", e => {
-    if(e.key === "r") {
-        square.style.backgroundColor = "red"
-    } else if (e.key === "g") {
-        square.style.backgroundColor = "green"
-    } else if (e.key === "o") {
-        square.style.backgroundColor = "orange"
-    } else if (e.key === "y") {
-        square.style.backgroundColor = "yellow"
-    } else if (e.key === "b") {
-        square.style.backgroundColor = "blue"
-    } else if (e.key === "t") {
-        square.style.backgroundColor = "turquoise"
+document.body.addEventListener("keydown", e => {
+    if(e.key === 'r') {
+        square.style.backgroundColor = 'red'
+    } else if (e.key === 'g') {
+        square.style.backgroundColor = 'green'
+    } else if (e.key === 'o') {
+        square.style.backgroundColor = 'orange'
+    } else if (e.key === 'y') {
+        square.style.backgroundColor = 'yellow'
+    } else if (e.key === 'b') {
+        square.style.backgroundColor = 'blue'
     }
     console.log ("Color Pressed");
+})
+
+document.body.addEventListener("keyup", e=> {
+    square.style.backgroundColor= "turquoise";
+    console.log ("Origin Color");
 })
