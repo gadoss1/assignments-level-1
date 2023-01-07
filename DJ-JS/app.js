cb88a1631cc704a90d41864ce90c266ce14cad27
Gregory Doss <gadoss83@yahoo.com>
2023-01-06 19:03:02 -0500

    Corrected DJ JS coding

--- DJ-JS/app.js
+++ DJ-JS/app.js
@@ -1,81 +1,51 @@
 // DJ-JS-Assignment //
 
-// Draw square
-function setup() {
-	
-	// Create Canvas of given size
-	createCanvas(300, 300);
+const square = document.querySelector(".square");
 
-}
-
-function draw() {
-	
-	background(220);
-	
-	// Use color() function
-	let c = color('white');
-
-	// Use fill() function to fill color
-	fill(c);
-	
-	// Draw a square
-	square(50, 50, 200);
-	
-}
-
-document.body.style.textAlign = "center";
-document.body.style.backgroundColor = "gray";
-
- function myFunction(DJBlue){
-    console.log("They changed to blue!");
-}
-
-document.getElementById("DJBlue").addEventListener('hover', function(color= 'blue'){
-    //console.log("Hover Blue!");
-
+square.addEventListener("mouseover", function(){
+    square.style.backgroundColor= "turquoise";
+    console.log ("Back to original");
 })
 
-function myFunction(DJRed){
-    console.log("They changed to red!");
-}
-
-document.getElementById("DJRed").addEventListener('hold', function(color= 'red'){
-    console.log("Hold Red!");
-
+square.addEventListener("click", function () {
+square.style.backgroundColor = "blue";
+console.log ("DJ Blue");
 })
 
-function myFunction(DJYellow){
-    console.log("They changed to yellow!");
-}
-
-document.getElementById("DJYellow").addEventListener('release', function(color= 'yellow'){
-    console.log("Released Yellow!");
-
+square.addEventListener("mousedown", function(){
+    square.style.backgroundColor= "red";
+    console.log ("DJ Red");
 })
 
-function myFunction(DJGreen){
-    console.log("Doubled up on green!");
-}
-
-document.getElementById("DJGreen").addEventListener('doubleclick', function(color= 'green'){
-    console.log("Clicked twice for Green!");
-
+square.addEventListener("mouseleave", function(){
+    square.style.backgroundColor= "yellow";
+    console.log ("DJ Yellow");
 })
 
-function myFunction(DJOrange){
-    console.log("Scrolling up orange!");
-}
-
-document.getElementById("DJOrange").addEventListener('onMouseWheel', function(color= 'orange'){
-    console.log("Scrolling stroll for orange!");
-
+square.addEventListener("dblclick", function(){
+    square.style.backgroundColor= "green";
+    console.log ("DJ Green");
 })
 
-//function myFunction(DJLetter){
-    //console.log("Changed letter by first letter of color!");
-//}
+document.body.addEventListener("scrollover", function(){
+    square.style.backgroundColor= "orange";
+    console.log ("DJ Orange");
+})
 
-//document.getElementById("DJLetter").addEventListener('press', function(color= 'blue', 'red', 'yellow', 'green', 'orange'){
-   // console.log("Color changed by color's first initial!");
 
-//})
+document.body.addEventListener("keyDown", e => {
+    if(e.key === "r") {
+        square.style.backgroundColor = "red"
+    } else if (e.key === "g") {
+        square.style.backgroundColor = "green"
+    } else if (e.key === "o") {
+        square.style.backgroundColor = "orange"
+    } else if (e.key === "y") {
+        square.style.backgroundColor = "yellow"
+    } else if (e.key === "b") {
+        square.style.backgroundColor = "blue"
+    } else if (e.key === "t") {
+        square.style.backgroundColor = "turquoise"
+    }
+    console.log ("Color Pressed");
+})
