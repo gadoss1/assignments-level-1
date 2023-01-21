d9004e2600dc1b8f5fd847b584059268c28459a9
Gregory Doss <gadoss83@yahoo.com>
2023-01-21 00:19:10 -0500

    DOM Debug assignment code work

--- DOM-Debug/app.js
+++ DOM-Debug/app.js
@@ -1,30 +1,47 @@
-const colors = ["red", "blue", "green"]
+const colors = ["red", "blue", "green"];
+const list = document.getElementById("list");
+
+form.addEventListener("click", function() {
+    //event.preventDefault();
+    
+    const userInput = add.value;
+    let newLi = document.createElement("li");
+    let newDiv = document.createElement("div");
+
+        //Add new item to list
+        newDiv.textContent = userInput;
+    
+        list.append(newLi);
+        list.insertBefore(newLi);
+        newLi.append(newDiv);
+
+})
 
 document.getElementById("add").addEventListener("onclick", function(e){
-    const subItem = createSubItem(e)
-    document.getElementById("list").appendChild(subItem)
+    const subItem = createSubItem(e);
+    document.getElementById("list").appendChild(subItem);
 })
 
 function createDropDown(){
-    const dropDown = document.createElement("select")
+    const dropDown = document.createElement("select");
     for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
-        option.innerHTML = colors[i]
-        option.value = colors[i]
-        dropDown.append(option)
+        const option = createElement("option");
+        option.innerHTML = colors[i];
+        option.value = colors[i];
+        dropDown.append(option);
     }
     dropDown.addEventListener("onchange", function(e){
         e.target.parent.backgroundColor = e.target.value
     })
-    return dropDown
+    return dropDown;
 }
 
 function createSubItem(e){
-    const subItem = document.createElement("div")
-    var subItemValue = document.getElementById("input")
-    subItem.textContent = subItemValue
-    const dropDown = createDropDown()
-    subItem.appendChild(dropDown)
-    subItem.setAttribute("class", "subItem")
-    return subItem
+    const subItem = document.createElement("div");
+    var subItemValue = document.getElementById("input");
+    subItem.textContent = subItemValue;
+    const dropDown = createDropDown();
+    subItem.appendChild(dropDown);
+    subItem.setAttribute("class", "subItem");
+    return subItem;
 }
