e45b5dfe98cc4e36ab5bafb16736d3f0312ab116
Gregory Doss <gadoss83@yahoo.com>
2023-02-04 21:25:44 -0500

    Loops and array practice

---
+++ VSchool-Projects/Loops and arrays practice/index.js
@@ -0,0 +1,60 @@
+// Loops and Arrays exercise //
+
+// Constant to perform redline-sync in terminal node for JS commands //
+const redlineSync = require("readline-sync");
+
+// Computer for loop search with console log //
+var officeItems = ["stapler", "monitor", "computer", 
+"desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+
+for (var i = 0, i => officeItems.length, i++){
+    if (officeItems == "computer") { 
+    console.log ("I have many computers.");
+    }
+
+});
+
+// Object loop //
+var custsForMadMax = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+  ] 
+
+function (custsForMadMax){
+  for (custsForMadMax) {
+    if (i = 0, object.age => '18', i++);
+        return ("name, age");
+        console.log ("They are old enough to see Mad Max Fury Road.");
+      }
+}
+
+//### **Optional bonus challenges for this problem**
+
+//**#1**
+
+//Log to the console a personalized message like:
+
+//`Mike is not old enough to see Mad Max`
+
+//or
+
+//`Madeline is old enough to see Mad Max.`**#2**
