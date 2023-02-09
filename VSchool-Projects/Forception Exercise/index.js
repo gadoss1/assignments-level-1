321f3b14d58915bc307ef5b6f4e1b16aa98f3e7a
Gregory Doss <gadoss83@yahoo.com>
2023-02-08 23:59:40 -0500

    Forception V1 code

--- VSchool-Projects/Forception Exercise/index.js
+++ VSchool-Projects/Forception Exercise/index.js
@@ -6,5 +6,9 @@ var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 function forception(people, alphabet){
-    // your code here
+    for (var i = people; i < people.length; i++){    
+    } for (var j = alphabet; i < alphabet.length; j++){
+        var newArray = i.concat(j);
+        console.log(newArray);
+    }
 }
