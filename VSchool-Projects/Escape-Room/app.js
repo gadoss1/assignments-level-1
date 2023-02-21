64e5f17fccf1f8d571cb1c7d89b0e4a778eeaa53
Gregory Doss <gadoss83@yahoo.com>
2023-02-21 17:06:12 -0500

    Escape Room quick create

--- VSchool-Projects/Escape-Room/app.js
+++ VSchool-Projects/Escape-Room/app.js
@@ -1,53 +1,192 @@
 // Escape Room Assignment //
 
-// Consts for Escape Room
+//Intro and require readline-sync.
 const readline = require("readline-sync");
-const name = readline.question("Welcome to your doom in the Ripper's escape room! What is your name? ");
-const age = readline.question("${name}, are you 17 yrs old and older to play? Y/N: ")
-const death = readline.question("Did you know you need the keys? Y/N: ");
-const keys = readline.question("Do you have the keys? Y/N: ");
-const spikeHandHole = readline.question("Would you like to put your hand in the hole? Y/N: ")
-const door = readline.question("Do you want to open the door? Y/N: ");
-const escape = "Welcome to your freedom! Game Over!";
-const playAgain = readline.question("Play again? Y/N: ");
-const escapeRoom = ();
+const name = readline.question("What is your name? ");
 
-// Printed Outputs
-console.log(name);
-console.log(age);
-console.log(spikeHandHole);
-console.log(keys);
-console.log(death);
-console.log(door);
+console.log(`Welcome, ${name}, to your doom in the Ripper's Escape Room!`);
 
-// Inputs and Logic
-while (escapeRoom){
-
-if (age = 'Y'){
-    console.log("Please start the game.");
-    else if (age == 'N') {
-        console.log("You are not old enough to play.");
-    }
-
+//Boolean flags tells when the player is dead, has found keys, and has pressed 1 to start. Variable for the room #.
+let isAlive = true;
+let hasKeys = false;
+let pressStart = false;
+let room = 1;
+const start = readline.keyIn("Please press 1 to start!", {limit: '$<1>'});
+if (start == 1)
+{
+    console.log("You awaken in a strange room. There is a small hole in the wall big enough for your hand. \nThe door is locked. There is a pantry at the back wall. \nWhat would you like to do?");
+    pressStart = true;
 }
 
-if (spikeHandHole == 'Y'){
-    console.log("Time to die and bleed out! Muhahah!" + playAgain);
-    else if (spikeHandHole == 'N'){
-        console.log("Good choice! Now go get the keys!");
+//While loop for Room #1.
+while (isAlive == true && pressStart == true && room == 1)
+{
+    const optionID = readline.keyIn("Press 1 to put your hand in the hole. \nPress 2 to search the pantry. \nPress 3 to open the door.", {limit: '$<1-3>'});
+    if (optionID == 1)
+    {
+        console.log("SOMETHING PUNCTURES YOUR HAND AND YOU BLEED OUT! YOU DIED!");
+        isAlive = false;
+    }
+    else if (optionID == 2)
+    {
+        if (hasKeys == true)
+        {
+            console.log("THERE IS NOTHING ELSE IN THE PANTRY. QUIT WASTING TIME AND SNACKING!");
+        }
+        else if (hasKeys == false)
+        {
+            console.log("YOU FOUND KEYS IN THE PANTRY!");
+            hasKeys = true;
+        }
+    }
+    else if (optionID == 3)
+    {
+        if (hasKeys == false)
+        {
+            console.log("YOU SHOULD TRY TO FIND THE KEYS FIRST!");
+        }
+        else if (hasKeys == true)
+        {
+            console.log("You find yourself in a new room. There is a purple glass door directly across from you. \nThe door has a mail slot in it. There is a pen and paper on a table in the center of the room. \nWhat would you like to do?");
+            room = 2;
+            break;
+        }
     }
 }
 
-if (keys == 'Y');
-    console.log("Please proceed to the door.");
-    else if (keys = 'N' && door == 'Y'){
-    console.log("You shall not pass!");
+//While loop for room #2.
+while (isAlive == true && room == 2)
+{
+    const optionID = readline.keyIn("Press 1 to search the room. \nPress 2 to look at the table. \nPress 3 to charge through the door.", {limit: '$<1-3>'});
+    if (optionID == 1)
+    {
+        console.log("OTHER THAN THE TABLE, PEN AND PAPER YOU DO NOT SEE ANYTHING ELSE!");
     }
-
-if (door = 'Y' + keys == 'Y'){
-    console.log(escape && playAgain);
-    else if (door = 'N' && keys == 'N'){
-    console.log("You need the key to escape your doom!");
+    else if (optionID == 2)
+    {
+        let lookAt = true;
+        console.log("On the table is a pen and a piece of paper. The paper says 'What do you think may enter my green glass door?'.");
+        while (lookAt == true)
+        {
+            const optionID2 = readline.keyIn("Press 1 to write on the paper and put it in the mail slot. \nPress 2 to go back.", {limit: '$<1-2>'});
+            if (optionID2 == 1)
+            {
+                const paperInput = readline.question("What do you want to write?: ");
+                for (i = 0; i < paperInput.length; i++)
+                {
+                    if (paperInput[i] == paperInput[i+1])
+                    {
+                        console.log("THE DOOR OPENED! \nYou enter a new room. A statue is standing in the center of the room. \nThere are pictures on one of the walls. \nWhat would you like to do?");
+                        lookAt = false;
+                        room = 3;
+                        break;
+                    }
+                    if (i == paperInput.length-1)
+                    {
+                        console.log("NOTHING HAPPENED! TRY AGAIN?");
+                        break;
+                    }
+                }
+            }
+            else if (optionID2 == 2)
+            {
+                console.log("YOU STEP AWAY FROM THE TABLE.")
+                lookAt = false;
+                break;
+            }
+        }
+    }
+    else if (optionID == 3)
+    {
+        console.log("OOPS! WHEN YOU CHARGED THROUGH THE DOOR THE GLASS IMPALED YOU! YOU'RE DEAD!");
+        isAlive = false;
     }
 }
+
+//While loop for room #3.
+while (isAlive == true && room == 3)
+{
+    let hitOne = false;
+    let hitTwo = false;
+    let hitThree = false;
+    const optionID = readline.keyIn("Press 1 to look at the pictures. \nPress 2 to look at the statue. \nPress 3 to try to open the door.", {limit: '$<1-3>'});
+    if (optionID == 1)
+    {
+        console.log("FROM LEFT TO RIGHT THE PICTURES ARE A STAR, A REVOLVER, A TIGER, A PAW PRINT.");
+    }
+    else if (optionID == 2)
+    {
+        let lookAt = true;
+        console.log("At the base of the statue is a compass. An engraving on the statue's base says \n'Point me in the direction and I'll show you the door. Steer me wrong and I'll show you the floor.' \nWhat would you like to do?");
+        while (lookAt == true)
+        {
+            const optionID2 = readline.keyIn("Press 1 to turn the statue north. \nPress 2 to turn the statue south. \nPress 3 to turn the statue east. \nPress 4 to turn the statue west. \nPress 5 to go back.", {limit: '$<1-5>'});
+            if (optionID2 == 1)
+            {
+                if (hitOne == false && hitTwo == false && hitThree == false)
+                {
+                    console.log("YOU HEAR A CLICK!");
+                    hitOne = true;
+                }
+                else
+                {
+                    console.log("THE STATUE EXPLODES! RIP!");
+                    isAlive = false;
+                    break;
+                }
+            }
+            else if (optionID2 == 2)
+            {
+                if (hitOne == true && hitTwo == true && hitThree == true)
+                {
+                    console.log("YOU HEAR A FINAL CLICK AS THE DOOR OPENS TO THE OUTSIDE! \nCONGRATULATIONS! YOU'RE FREE!");
+                    room = 4;
+                    break;
+                }
+                else
+                {
+                    console.log("THE STATUE EXPLODES! RIP!");
+                    isAlive = false;
+                    break;
+                }
+            }
+            else if (optionID2 == 3)
+            {
+                if (hitOne == true && hitTwo == true && hitThree == false)
+                {
+                    console.log("YOU HEAR A CLICK!");
+                    hitThree = true;
+                }
+                else
+                {
+                    console.log("THE STATUE EXPLODES! RIP!");
+                    isAlive = false;
+                    break;
+                }
+            }
+            else if (optionID2 == 4)
+            {
+                if (hitOne == true && hitTwo == false)
+                {
+                    console.log("YOU HEAR A CLICK!");
+                    hitTwo = true;
+                }
+                else
+                {
+                    console.log("THE STATUE EXPLODES! RIP!");
+                    isAlive = false;
+                    break;
+                }
+            }
+            else if (optionID2 == 5)
+            {
+                console.log("YOU STEP AWAY FROM THE STATUE. IT STARTS CLICKING AS IT RESETS ITSELF!");
+                lookAt = false;
+            }
+        }
+    }
+    else if (optionID == 3)
+    {
+        console.log("THE DOOR IS LOCKED! YOU ARE FOREVER ENTOMBED AND DOOMED!");
+    }
 }
