ef90c258353d37d9c69077fb4864907f841d3591
Gregory Doss <gadoss83@yahoo.com>
2023-02-21 12:35:15 -0500

    Escape Room assignment start

--- VSchool-Projects/Employee-Records/index.js
+++ VSchool-Projects/Employee-Records/index.js
@@ -9,6 +9,9 @@ function Employee (firstName, lastName, jobTitle, salary, status ) {
     this.jobTitle = jobTitle;
     this.salary = salary;
     this.status = status;
+    this.print = function () {
+        console.log(JSON.stringify(this, null, 5));
+    }
 }
 
 // Employees
@@ -17,10 +20,16 @@ var emp2 = new Employee("Jason", "Saver", "EMT", 60000, "Full-Time");
 var emp3 = new Employee("Jack", "Axe", "Firefighter", 55000, "Full-Time");
 var emp4 = new Employee("Jill", "Pretty", "Nurse", 45000, "Part-Time");
 
+// Prints individual employee records
+emp1.print();
+emp2.print();
+emp3.print();
+emp4.print();
+
 // Pushes employee constructor information into empty company employees array
 employees.push(emp1, emp2, emp3, emp4);
 console.log(employees);
 
 // Change Job Status
-Employee.prototype.status = "Contract";
+emp4.status = "Contract";
 console.log (emp4);
---
+++ VSchool-Projects/Escape-Room/app.js
@@ -0,0 +1,53 @@
+// Escape Room Assignment //
+
+// Consts for Escape Room
+const readline = require("readline-sync");
+const name = readline.question("Welcome to your doom in the Ripper's escape room! What is your name? ");
+const age = readline.question("${name}, are you 17 yrs old and older to play? Y/N: ")
+const death = readline.question("Did you know you need the keys? Y/N: ");
+const keys = readline.question("Do you have the keys? Y/N: ");
+const spikeHandHole = readline.question("Would you like to put your hand in the hole? Y/N: ")
+const door = readline.question("Do you want to open the door? Y/N: ");
+const escape = "Welcome to your freedom! Game Over!";
+const playAgain = readline.question("Play again? Y/N: ");
+const escapeRoom = ();
+
+// Printed Outputs
+console.log(name);
+console.log(age);
+console.log(spikeHandHole);
+console.log(keys);
+console.log(death);
+console.log(door);
+
+// Inputs and Logic
+for (escapeRoom){
+
+if (age = 'Y'){
+    console.log("Please start the game.");
+    else if (age == 'N') {
+        console.log("You are not old enough to play.");
+    }
+
+}
+
+if (spikeHandHole == 'Y'){
+    console.log("Time to die and bleed out! Muhahah!" + playAgain);
+    else if (spikeHandHole == 'N'){
+        console.log("Good choice! Now go get the keys!");
+    }
+}
+
+if (keys == 'Y');
+    console.log("Please proceed to the door.");
+    else if (keys = 'N' && door == 'Y'){
+    console.log("You shall not pass!");
+    }
+
+if (door = 'Y' + keys == 'Y'){
+    console.log(escape && playAgain);
+    else if (door = 'N' && keys == 'N'){
+    console.log("You need the key to escape your doom!");
+    }
+}
+}
