de8108520029d93750420c3f223c720e00edc612
Gregory Doss <gadoss83@yahoo.com>
2023-03-06 20:54:30 -0500

    HTML Calculator work

--- Level2Work/HTMLCalculator/index.js
+++ Level2Work/HTMLCalculator/index.js
@@ -1,26 +1,34 @@
 // Wanted Functions with Divs and Forms
-const add = document["form1"]
-const subtract = document["form2"]
-const multiply = document["form3"]
+const add = document.getElementById("form1")
+const subtract = document.getElementById("form2")
+const multiply = document.getElementById("form3")
 
 // Submit event
 
 form.addEventListener("submit", function(event){
     event.preventDefault()
-        
-    const adds = [form1.num1.value] + [form1.num2.value]
-    const subtracts = [form2.num1.value] - [form2.num1.value]
-    const muliplying = [form3.num1.value] * [form3.num2.value]
-    form.num1.value = ""
-    form.num2.value = ""
+    
+    // Add Numbers and Result Functions
+    const adds = ([form1.num1.value], [form1.num2.value])
+    const subtracts = ([form2.num1.value] - [form2.num1.value])
+    const muliplying = ([form3.num1.value] * [form3.num2.value])
+    form1.num1.value = ""
+    form1.num2.value = ""
+    form2.num1.value = ""
+    form2.num2.value = ""
+    form3.num1.value = ""
+    form3.num2.value = ""
     let addResult = adds.value
     let subtractResult = subtracts.value
     let multiplyResult = muliplying.value
+    console.log(addResult)
+    console.log(subtractResult)
+    console.log(multiplyResult)
     
-    // 1 . <h1></h1>
+    // Create h1
     const h1 = document.createElement('h1')
-    // 2. <h1>Math Results</h1>
+    // 2. Math Function Results
     h1.textContent = addResult, subtractResult, multiplyResult
-    // 3. Append
+    // 3. Append values to DOM body
     document.getElementsByTagName("body")[0].append(h1)
 })
